95c5b02deb8264e4af310edaf39aeb8548fa71a8
Sameer Jha <68643572+sameerjha18@users.noreply.github.com>
2021-09-29 21:02:55 +0530

    commenting out my code

--- rss.py
+++ rss.py
@@ -3,24 +3,46 @@ from urllib.request import urlopen
 
 from bs4 import BeautifulSoup as soup
 
+#setting news_url to from where we are fetching news
 news_url = "https://news.google.com/news/rss"
 
 app = Flask(__name__)
 
 
 def get_news_from_google():
+    # using urlopen function and giving him the news_url and storing it into client
     client = urlopen(news_url)
     page = client.read()
+
+    #now we are using read function and reading the client data and setting it into page
     client.close()
+
     souped = soup(page, "xml")
+    #now we are using beautifulsoup and giving page and setting it into xml
+
     news_list = souped.findAll("item")
+    #now geeting item form souped variable with help of findall func and store in varibale
+
     result = []
+    #now creating blank list
+
     for news in news_list:
+        #looping through the news_list
+
         data = {}
+        # blank dictionary
+
         data['title'] = news.title.text
+        #storing title of news getiing from looping and storing in dictionary
+
         data['date'] = news.pubDate.text
+        #same as above
+
         result.append(data)
+        #now adding data into list using apend method
+
     return result
+    #returnig the list
 
 
 @app.route('/')
